f87da847b422e5c0abd2fa468c6aed22ba44fcda
Rohil Gugunta <108376638+RGcoderA@users.noreply.github.com>
2024-05-21 21:24:41 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App{
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!");
+
+        String name = "Mike Tyson";
+        char testgrade ='A';    // unicode 16 bit  
+
+
+
+
+        System.out.print(name + testgrade);
+
+    }
+}
